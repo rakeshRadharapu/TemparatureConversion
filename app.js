85bbee25f2163b35fcc80340c082205ef86d0cde
rakeshRadharapu <radharapurakesh4@gmail.com>
2025-06-04 15:55:25 +0530

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-let username;
-document.getElementById("sub-btn").onclick=function(){
-    username = document.getElementById("ipt").value;
-    console.log("username");
-    document.getElementById("h1").textContent=username;
-}
